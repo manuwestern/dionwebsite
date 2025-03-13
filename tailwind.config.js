8fadbf618f822b2e6ff81a35934e1b7dfef2dfdf
manuwestern <manuel.bayer@hotmail.de>
2025-03-13 10:36:40 +0100

    Initial commit with hero section benefits

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1a1a1a',
+      },
+    },
+  },
+  plugins: [],
+};
