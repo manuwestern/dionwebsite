1b46682f99c7e45cc5812fe0cbe4f93bbbc820b7
manuwestern <manuel.bayer@hotmail.de>
2025-03-14 12:41:02 +0100

    Increase method card height to eliminate scrollbars while maintaining alignment

--- src/components/hairTransplantation/MethodsSection.tsx
+++ src/components/hairTransplantation/MethodsSection.tsx
@@ -39,17 +39,17 @@ const MethodsSection: React.FC = () => {
               </div>
               
               {/* Content container with fixed height and absolute positioning for sections */}
-              <div className="p-4 bg-white relative h-[650px]">
+              <div className="p-4 bg-white relative h-[800px]">
                 {/* Description section - fixed position */}
-                <div className="absolute top-4 left-4 right-4 h-[100px] overflow-y-auto">
+                <div className="absolute top-4 left-4 right-4 h-[120px]">
                   <p className="text-gray-700 font-light">
                     {method.description}
                   </p>
                 </div>
                 
                 {/* Benefits section - fixed position */}
-                <div className="absolute top-[120px] left-4 right-4 h-[220px] overflow-y-auto">
-                  <h4 className="text-lg font-medium mb-4 sticky top-0 bg-white z-10">Vorteile:</h4>
+                <div className="absolute top-[140px] left-4 right-4 h-[280px]">
+                  <h4 className="text-lg font-medium mb-4">Vorteile:</h4>
                   <ul className="space-y-3">
                     {method.benefits.map((benefit: string, i: number) => (
                       <li key={i} className="flex items-start">
@@ -63,8 +63,8 @@ const MethodsSection: React.FC = () => {
                 </div>
                 
                 {/* Process section - fixed position */}
-                <div className="absolute top-[360px] left-4 right-4 h-[270px] overflow-y-auto">
-                  <h4 className="text-lg font-medium mb-4 sticky top-0 bg-white z-10">Prozess:</h4>
+                <div className="absolute top-[440px] left-4 right-4 h-[340px]">
+                  <h4 className="text-lg font-medium mb-4">Prozess:</h4>
                   <ol className="space-y-4">
                     {method.process.map((step: string, i: number) => (
                       <li key={i} className="flex items-start">
