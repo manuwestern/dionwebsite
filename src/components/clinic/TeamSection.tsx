4c3816a3d34857b40e3bd7e833fa28663f0c7d0b
manuwestern <manuel.bayer@hotmail.de>
2025-04-04 10:19:48 +0200

    Refactor TeamSection to adjust grid layout and update team member details

--- src/components/clinic/TeamSection.tsx
+++ src/components/clinic/TeamSection.tsx
@@ -31,7 +31,7 @@ const TeamSection: React.FC = () => {
         </div>
 
         {/* Team Members Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {teamMembers.map((member, index) => {
             const isHovered = index === hoverTeamMember;
 
