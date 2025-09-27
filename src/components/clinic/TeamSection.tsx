ccf2b8d3566f2bebd524b9928c3e9b29e4ef6929
manuwestern <manuel.bayer@hotmail.de>
2025-09-27 13:56:55 +0200

    Layout Anpassung Klinikseite

--- src/components/clinic/TeamSection.tsx
+++ src/components/clinic/TeamSection.tsx
@@ -30,7 +30,7 @@ const TeamSection: React.FC = () => {
         </div>
 
         {/* Team Members Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto">
           {teamMembers.map((member, index) => {
             const isHovered = index === hoverTeamMember;
 
